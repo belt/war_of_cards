0bba5ef2b1ec2e7f65ab6bdea700ad24e908656a
Paul Belt <paul.belt+github@gmail.com>
2025-08-12 15:23:12 -0400

    feat: drawing cards removes cards from hands

--- lib/war_of_cards/round.rb
+++ lib/war_of_cards/round.rb
@@ -5,5 +5,12 @@ module WarOfCards
     def initialize(players: Set.new)
       @players = Set.new(players || [])
     end
+
+    def cards_in_play
+      @cards_in_play ||= players.each_with_object(Set.new) do |player, acc|
+        card = player.draw_cards
+        acc << { player: player, card: card }
+      end
+    end
   end
 end
--- spec/war_of_cards/round_spec.rb
+++ spec/war_of_cards/round_spec.rb
@@ -1,15 +1,16 @@
 require "spec_helper"
 
 RSpec.describe WarOfCards::Round do
-  subject(:loosing_player) { (round.players - [winning_player]).sample }
+  subject(:round) { described_class.new(players: game.players) }
 
   let(:game) { WarOfCards::Game.new }
-
-  let(:round) { described_class.new(players: game.players) }
-
-  let(:winning_player) { round.players.sample }
+  let(:winning_player) { round.players.to_a.sample }
+  let(:loosing_player) { (round.players - [winning_player]).to_a.sample }
 
   before do
+    allow(game).to receive(:players).and_return(Set.new(
+      [winning_player, loosing_player]
+    ))
     allow(game).to receive(:current_round).and_return(round)
   end
 
@@ -19,7 +20,12 @@ RSpec.describe WarOfCards::Round do
         expect(round.players.size).to eq(game.players.size)
       end
 
-      it "players draw cards from respective hands", skip: "TODO" do
+      it "drawing cards removes cards from hands", :aggregate_failures do
+        round.cards_in_play.each do |player_hand|
+          expect(
+            round.cards_in_play.map { |play| play[:player] }.map(&:hand) & player_hand[:card]
+          ).to be_empty
+        end
       end
     end
   end
