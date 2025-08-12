dc62cd5b5146f0753add1b00f9575d3293664878
Paul Belt <paul.belt+github@gmail.com>
2025-08-12 12:40:17 -0400

    feat: init player count to 2 or 4 (happy path)
    prove rspec env
    
    TODO: init 3, init -1, init Tod

--- lib/war_of_cards/game.rb
+++ lib/war_of_cards/game.rb
@@ -1,4 +1,9 @@
 module WarOfCards
   class Game
+    attr_reader :player_count
+
+    def initialize(player_count: 2)
+      @player_count = player_count
+    end
   end
 end
--- spec/war_of_cards/game_spec.rb
+++ spec/war_of_cards/game_spec.rb
@@ -1,1 +1,29 @@
 require "spec_helper"
+
+RSpec.describe WarOfCards::Game do
+  describe "allows for 2 or 4 players" do
+    subject(:game) { described_class.new }
+
+    context "when initalizing with defaults" do
+      it "player_count is 2" do
+        expect(game.player_count).to eq(2)
+      end
+    end
+
+    context "when initializing to 4" do
+      subject(:game) { described_class.new(player_count: 4) }
+
+      it "player_count is 4" do
+        expect(game.player_count).to eq(4)
+      end
+    end
+
+    context "when initializing to other" do
+      subject(:game) { described_class.new(player_count: 3) }
+
+      it "throws an error" do
+        expect { game }.to raise_error(WarOfCards::Game::Error)
+      end
+    end
+  end
+end
