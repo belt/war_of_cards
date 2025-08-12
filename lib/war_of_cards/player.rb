63fe6b70057f314b60ec9f2d734fcde883b7416e
Paul Belt <paul.belt+github@gmail.com>
2025-08-12 13:08:25 -0400

    feat: player has deck

--- lib/war_of_cards/player.rb
+++ lib/war_of_cards/player.rb
@@ -1,4 +1,9 @@
 module WarOfCards
   class Player
+    attr_reader :deck
+
+    def initialize(deck: Set.new)
+      @deck = deck || Set.new
+    end
   end
 end
--- spec/war_of_cards/player_spec.rb
+++ spec/war_of_cards/player_spec.rb
@@ -1,1 +1,13 @@
 require "spec_helper"
+
+RSpec.describe WarOfCards::Player do
+  describe "player has deck" do
+    subject(:player) { described_class.new }
+
+    context "when initializing with defaults" do
+      it "has a deck" do
+        expect(player.deck).to be_a(Set)
+      end
+    end
+  end
+end
