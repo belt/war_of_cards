56ed21cb7d479a76d2aa08345eebd19246c89811
Paul Belt <paul.belt+github@gmail.com>
2025-08-12 15:00:05 -0400

    feat: player draws card

--- lib/war_of_cards/player.rb
+++ lib/war_of_cards/player.rb
@@ -5,5 +5,11 @@ module WarOfCards
     def initialize(hand: Set.new)
       @hand = hand || Set.new
     end
+
+    def draw_cards(batch_count: 1)
+      return hand.shift(hand.size) if hand.size <= batch_count
+
+      hand.shift(batch_count)
+    end
   end
 end
--- spec/war_of_cards/player_spec.rb
+++ spec/war_of_cards/player_spec.rb
@@ -9,5 +9,34 @@ RSpec.describe WarOfCards::Player do
         expect(player.hand).to be_a(Set)
       end
     end
+
+    describe "#draw_cards" do
+      subject(:player) { described_class.new(hand: cards) }
+
+      let(:cards) do
+        [
+          WarOfCards::Game::Card.new("A", "hearts"),
+          WarOfCards::Game::Card.new("K", "spades")
+        ]
+      end
+      let(:cards_to_draw) { 1 }
+      let(:cards_drawn) { player.draw_cards(batch_count: cards_to_draw) }
+
+      before do
+        player.hand = cards.dup
+      end
+
+      it "draws specified number of cards" do
+        expect(cards_drawn).to eq([cards.first])
+      end
+
+      context "when count is higher" do
+        let(:cards_to_draw) { 5 }
+
+        it "draws all remaining cards" do
+          expect(cards_drawn).to eq(cards)
+        end
+      end
+    end
   end
 end
